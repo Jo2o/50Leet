2f833c8995bf057fff270404d54b1aab4d844d67
Jozef Mikus <jozef.mikus@erstegroup.com>
2025-03-14 22:34:07 +0100

    initial

---
+++ src/main/kotlin/01-WaterLineTanks/main.kt
@@ -0,0 +1,9 @@
+package org.jo2o.fiftyleet.`01-WaterLineTanks`
+
+fun main() {
+    val name = "Kotlin"
+    println("Hello, " + name + "!")
+    for (i in 1..5) {
+        println("i = $i")
+    }
+}
