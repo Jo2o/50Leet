744c430ce75f25a3deee92bdbdcd14b3d8d895df
Jozef Mikus <jozef.mikus@erstegroup.com>
2025-03-21 11:29:58 +0100

    number palindrome

---
+++ src/main/kotlin/99-NumberPalindrome/memain.kt
@@ -0,0 +1,16 @@
+package org.jo2o.fiftyleet.`99-NumberPalindrome`
+
+fun main() {
+    println(isPalindrome(202))
+}
+
+private fun isPalindrome(x: Int): Boolean {
+    if (x < 0) return false
+    if (x == 0) return true
+    val str = x.toString()
+    val strLen = str.length
+    for (i in 0 until strLen/2) {
+        if (str[i] != str[strLen - i - 1]) return false
+    }
+    return true
+}
